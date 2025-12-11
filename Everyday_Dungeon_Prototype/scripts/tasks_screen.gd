46131d6c69da578aa3c8095f1edbd653359f6d7b
Darwin Deschenes <ddeschenes02@icloud.com>
2025-12-10 20:23:08 -0800

    new commit

---
+++ Everyday_Dungeon_Prototype/scripts/tasks_screen.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+@onready var add_button: Button = $TaskSystem/TaskMenu/CreateButton
+@onready var delete_button: Button = $TaskSystem/TaskMenu/DeleteButton
+@onready var line_edit: LineEdit = $TaskSystem/TaskMenu/LineEdit
+@onready var item_list: ItemList = $TaskSystem/TaskMenu/ItemList
+
+func _ready() -> void:
+	# Start with Add visible and Delete visible
+	add_button.visible = true
+	delete_button.visible = true
+	
+	item_list.connect("item_selected", Callable(self, "_on_item_selected"))
+	
+
+func _on_goals_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Screens/goals_screen.tscn")
+
+func _on_tasks_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Screens/tasks_screen.tscn")
+
+func _on_home_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Screens/home_screen.tscn")
+
+func _on_battle_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Screens/battle_screen.tscn")
+
+func _on_creatures_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Screens/creatures_screen.tscn")
+
+func _on_add_button_pressed() -> void:
+	var text = line_edit.text.strip_edges()
+	if text == "":
+		return
+
+	# Add new task
+	item_list.add_item(text)
+	line_edit.clear()
+
+
+func _on_delete_button_pressed() -> void:
+	print("Clicked")
+	var selected = item_list.get_selected_items()
+	if selected.size() == 0:
+		return
+		
+	#read task
+	var index = selected[0]
+	#get task type
+	var task_type: String = item_list.get_item_metadata(index)
+	#add reward
+	GlobalData.add_xp_task_type(task_type)
+	# Remove selected item
+	item_list.remove_item(index)
+
+	# Switch buttons
+	delete_button.visible = false
+	add_button.visible = true
+
+
+func _on_item_selected(_index: int) -> void:
+	add_button.visible = false
+	delete_button.visible = true
+	
+	
